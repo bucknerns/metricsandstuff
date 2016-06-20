31b381ba31763322fc9ffd2c1179feb856ce5769
Nathan Buckner <nathan.buckner@rackspace.com>
2016-06-20 07:59:24 -0500

    added dependencies for subunit2redis

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     long_description='{0}'.format(open('README.rst').read()),
     author='Nathan Buckner',
     install_requires=[
-        "falcon", "redis", "pytz", "python-dateutil", "requests"],
+        "falcon", "redis", "pytz", "python-dateutil", "requests", "multiprocess", "subunit2sql"],
     packages=find_packages(exclude=('tests*', 'docs')),
     classifiers=(
         'Development Status :: 4 - Beta',
